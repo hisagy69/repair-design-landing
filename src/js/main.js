a68bae83d7c9864120e7d1f7db45911da938ba5a
sinegamy11 <sinegamy11@gmail.com>
2023-08-15 10:27:08 +0700

    feat: add map

--- src/js/main.js
+++ src/js/main.js
@@ -133,4 +133,20 @@ $(function() {
     });
     // маска телефона
     $('[type="tel"').mask('+7-000-000-0000');
+    // карта
+    const init = () => {
+        var myMap = new ymaps.Map("map", {
+            center: [47.244642, 39.723191],
+            zoom: 17,
+            controls: []
+        });
+        const myPlacemark = new ymaps.Placemark([47.244642, 39.723191], {}, {
+            iconLayout: 'default#image',
+            iconImageHref: '../img/marker.svg',
+            icon_imagesize: [50, 65],
+            iconImageOffset: [-3, -42]
+        });
+        myMap.geoObjects.add(myPlacemark);
+    }
+    ymaps.ready(init);
 });
